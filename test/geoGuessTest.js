f03c52a7d57b611e5d3fd280a36ebf190bf1598c
Paul Beaudet <inof8or@gmail.com>
2020-11-30 17:04:03 -0500

    More failing test for basic search queries

--- test/geoGuessTest.js
+++ test/geoGuessTest.js
@@ -1,25 +1,39 @@
 // geoGuessTest.js Copyright 2020 Paul Beaudet MIT License
 
 const { geoGuess } = require('../geoGuess');
-let testStatus = 'fail';
+
+const expectResults = (query, number = 1) => {
+  testStatus = 'fail';
+  try {
+    const json = await geoGuess(query);
+    const regex = new RegExp(query, 'g');
+    for(let i = 0; i < number; i++){
+      if(testStatus = json?.results?.length === 1 && json.results[i].name.search(regex) === 0){
+        testStatus = 'success';
+      } else {
+        return 'fail';
+      }
+    }
+  } catch (error){
+    console.log(`${error}`);
+  }
+  return testStatus;
+}
 
 const findOnExactName = async () => {
-  try {
-    const json = await geoGuess('Abbotsford');
-    testStatus = json?.results?.length === 1 && json.results[0].name === 'Abbotsford'
-      ? 'success'
-      : 'fail'
-  } catch (error){
-    testStatus = 'fail';
-  }
+  const testStatus = expectResults('Abbotsford');
   console.log(`it can find a place based on exact name: ${testStatus}`);
 };
 
 const findOnPartOfName = async () => {
-  console.log(`it can find a few unique places based on search parts: ${testStatus}`);
+  let testStatus = expectResult('Abbo');
+  console.log(`it can find a one unique place based on search parts: ${testStatus}`);
+  testStatus = expectResults('Pré');
+  console.log(`it can find another unique place based on search parts: ${testStatus}`);
 };
 
 const findMultipleResults = async () => {
+  const testStatus = expectResults('Bea', 3);
   console.log(`it gives results for search parts that would have multiple possibilities: ${testStatus}`);
 };
 
