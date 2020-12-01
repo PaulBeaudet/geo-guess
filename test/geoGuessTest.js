32ebb37535b6a30439efde97f6061c1f189ae750
Paul Beaudet <inof8or@gmail.com>
2020-12-01 16:39:24 -0500

    It can respond to a query with no result

--- test/geoGuessTest.js
+++ test/geoGuessTest.js
@@ -21,6 +21,25 @@ const expectResults = (msg, query, number = 1) => {
   }, query);
 };
 
+const expectUniqueResults = (msg, query, number = 1) => {
+  let testStatus = 'success';
+  geoGuess((json) => {
+    if(!json?.results?.length){
+      console.log(`${msg}: fail => no results`);
+      return;
+    }
+    for(let i = 0; i < json.results.length; i++){
+      for(let j = 0; j < json.results.length; j++){
+        if(json.results[i].name === json.results[j].name){
+          testStatus = 'fail';
+          break;
+        }
+      }
+    }
+    console.log(`${msg}: ${testStatus}`);
+  }, query);
+};
+
 const expectNoResults = (msg, query) => {
   let testStatus = 'fail';
   geoGuess((json) => {
@@ -35,6 +54,7 @@ const allTheTest = async () => {
   expectResults(`it can find another unique place based on search parts`, 'Pré');
   expectResults(`it gives results for search parts having multiple possibilities`, 'Bea', 20);
   expectNoResults(`it responds even when there are no results`, 'Zz');
+  expectUniqueResults(`It gives unique results for a common place name`, 'Auburn');
 };
 
 allTheTest();
